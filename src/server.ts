a750cf926647d296a5b0ec1e337bee5e1023d902
Kingsquare BV <source@kingsquare.nl>
2020-02-08 16:53:01 +0100

    Set up RaspividStreamer

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
+import http from "http";
 import express, { Request, Response } from "express";
+import RaspividStreamer from "./inc/raspividStreamer";
 
 const app = express();
+const server = http.createServer(app);
+
+const raspividStreamer = new RaspividStreamer({
+  fps: 12,
+  width: 960,
+  height: 540,
+  port: 8080
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ hello: "world" });
 });
 
-app.listen(80, () => {
-  console.log("Draadbuiger API listening on port ", 80);
+server.listen(80, () => {
+  console.log("Draadbuiger API listening on port", 80);
+  console.log("Raspivid streamer configured as ", raspividStreamer.options);
 });
